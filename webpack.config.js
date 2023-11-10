e54b4e041365921f51f089d7410f3887fff3b140
reworldcreative <reworldcreative@gmail.com>
2023-11-10 15:44:10 +0200

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,12 @@ module.exports = {
   optimization: isProduction
     ? {
         minimize: true,
+        runtimeChunk: "single",
         splitChunks: {
           chunks: "all",
         },
         usedExports: true,
+        moduleIds: "deterministic",
         minimizer: [
           new TerserPlugin({
             terserOptions: {
@@ -169,6 +171,9 @@ module.exports = {
 
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
   },
 
   plugins: [
